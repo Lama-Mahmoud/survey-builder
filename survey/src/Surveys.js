46b68157ba12d39b7017df30af23b7baa3ec0ef2
lama mahmoud <lamamahmoud213@gmail.com>
2022-06-19 02:58:55 +0300

    Finishing home page for users front-end

---
+++ survey/src/Surveys.js
@@ -0,0 +1,15 @@
+import './surveys.css';
+const Surveys = () => {
+    return ( 
+        <div className='cards'>
+            <div className='card' align="center">
+                <p>Title</p>
+                <button className='Button'>Take Survey</button>
+            </div>
+
+            
+        </div>
+     );
+}
+ 
+export default Surveys;
--- survey/src/index.js
+++ survey/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Login';
+import Surveys from './Surveys';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login/>
+    <Surveys/>
   </React.StrictMode>
 );
 
